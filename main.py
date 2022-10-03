a282b45e4034296404b3944d58832452cc64c31e
chaeyeon <yeonoey0914@gmail.com>
2022-10-03 14:49:46 +0900

    Updates 2 on Github Only

--- main.py
+++ main.py
@@ -21,3 +21,6 @@ print("Hello World Updates on Github")
 
 #Updates in local master branch
 print("Hello World Updates on Local Master Branch")
+
+#Updates on mainbranch of Github
+print("Hello World Updates 2 on Github Only")
